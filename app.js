7bc91b96bb3e3de7a830dff67f85c0fc10e1b19e
Imac-home <imac-home@iMac-Imac-home.local>
2018-12-22 01:47:58 +0200

    Startrek

---
+++ app.js
@@ -0,0 +1,12 @@
+let characters = [
+	{grade:"seniorOfficer", name: "Jean-Luc Picard" ,rank:"" ,gender:"male", species:"", affiliation:"", occupation:"", serialNumber:"", status:"", born:"", img:""},
+	{grade:"", name: "" ,rank:"" ,gender:"", species:"", affiliation:"", occupation:"", serialNumber:"", status:"", born:"", img:""},
+	{grade:"", name: "" ,rank:"" ,gender:"", species:"", affiliation:"", occupation:"", serialNumber:"", status:"", born:"", img:""},
+	{grade:"", name: "" ,rank:"" ,gender:"", species:"", affiliation:"", occupation:"", serialNumber:"", status:"", born:"", img:""},
+	{grade:"", name: "" ,rank:"" ,gender:"", species:"", affiliation:"", occupation:"", serialNumber:"", status:"", born:"", img:""},
+	{grade:"", name: "" ,rank:"" ,gender:"", species:"", affiliation:"", occupation:"", serialNumber:"", status:"", born:"", img:""},
+	{grade:"", name: "" ,rank:"" ,gender:"", species:"", affiliation:"", occupation:"", serialNumber:"", status:"", born:"", img:""},
+	{grade:"", name: "" ,rank:"" ,gender:"", species:"", affiliation:"", occupation:"", serialNumber:"", status:"", born:"", img:""},
+	{grade:"", name: "" ,rank:"" ,gender:"", species:"", affiliation:"", occupation:"", serialNumber:"", status:"", born:"", img:""},
+	{grade:"", name: "" ,rank:"" ,gender:"", species:"", affiliation:"", occupation:"", serialNumber:"", status:"", born:"", img:""}]
+
